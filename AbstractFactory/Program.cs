cc5409f193fd57846781fbc0d0bcb953b02d235c
Vladyslav Vasylets <Vladyslav Vasylets>
2017-06-10 18:52:29 +0300

    Add Abstract Factory

---
+++ AbstractFactory/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace AbstractFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var samsungFactory = new SamsungFactory();
+            var phone = samsungFactory.GetPhone();
+            var tablet = samsungFactory.GetTablet();
+
+            Console.WriteLine($"Phone - {phone.Name}\nTablet - {tablet.Name}");
+        }
+    }
+
+    public interface IFactory
+    {
+        Phone GetPhone();
+        Tablet GetTablet();
+    }
+
+    public class AppleFactory : IFactory
+    {
+        public Phone GetPhone()
+        {
+            return new IPhone();
+        }
+
+        public Tablet GetTablet()
+        {
+            return new IPad();
+        }
+    }
+
+    public class SamsungFactory : IFactory
+    {
+        public Phone GetPhone()
+        {
+            return new SamsungS8();
+        }
+
+        public Tablet GetTablet()
+        {
+            return new SamsungTab4();
+        }
+    }
+
+    public abstract class Phone
+    {
+        public string Name {get; set; }
+    }
+
+    public class IPhone: Phone
+    {
+        public IPhone()
+        {
+            this.Name = "Samsung Galaxy S8.";
+        }
+    }
+
+    public class SamsungS8: Phone
+    {
+        public SamsungS8()
+        {
+            this.Name = "Samsung Galaxy S8.";
+        }
+    }
+
+    public abstract class Tablet
+    {
+        public string Name { get; set; }
+    }
+
+    public class IPad: Tablet
+    {
+        public IPad()
+        {
+            this.Name = "Apple Ipad Pro 12\".";
+        }
+    }
+
+    public class SamsungTab4: Tablet
+    {
+        public SamsungTab4()
+        {
+            this.Name = "Samsung Galaxy Tab 4.";
+        }
+    }
+}
