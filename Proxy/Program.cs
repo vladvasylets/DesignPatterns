45dcffcc9d8b3a947da3079ec2ab8d53a607c444
Vladyslav Vasylets <Vladyslav Vasylets>
2017-06-13 23:23:08 +0300

    Add Proxy

---
+++ Proxy/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using static System.Console;
+
+namespace Proxy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var robot = new RobotBombDestructor();
+            var proxy = new RobotBombDestructorProxy(robot);
+
+            proxy.FindBomb();
+            proxy.Destruct();
+        }
+    }
+
+    public class RobotBombDestructor
+    {
+        public virtual void FindBomb()
+        {
+            WriteLine("Find bomb.");
+        }
+
+        public virtual void Destruct()
+        {
+            WriteLine("Bomb is destructed.");
+        }
+    }
+
+    public class RobotBombDestructorProxy: RobotBombDestructor
+    {
+        RobotBombDestructor _robot;
+
+        public RobotBombDestructorProxy(RobotBombDestructor robot)
+        {
+            this._robot = robot;
+        }
+
+        public override void FindBomb()
+        {
+            WriteLine("Use proxy to manage a robot.");
+            this._robot.FindBomb();
+        }
+
+        public override void Destruct()
+        {
+            WriteLine("Use proxy to manage a robot.");
+            this._robot.Destruct();
+        }
+    }
+}
